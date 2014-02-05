d584b3e92fd35c2dc1dd8038dbe00f4373b4d9ed
Spencer Rogers <spencer1248@gmail.com>
2014-02-05 13:27:32 -0500

    Initial commit

---
+++ animal_sorter.rb
@@ -0,0 +1,9 @@
+# Build a class AnimalSorter that accepts a list of animals on 
+# initialization.
+
+# Define a to_a method to account for the species in the test suite.
+
+# Return an array that contains two arrays, the first one
+# should include the sea creatures, the second, land animals.
+
+# Read the test suite for an example of a nested array.
---
+++ spec/animal_sorter_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "AnimalSorter" do
+  describe '#to_a' do
+    animals = ["marlin", "aardvark", "octopus", "cat", "fish", "elephant"]
+
+    it 'sorts sea and land animals' do
+      sorted_animals = [
+        ["marlin", "octopus", "fish"],
+        ["aardvark", "cat", "elephant"]
+      ]
+
+      expect(AnimalSorter.new(animals).to_a).to eq(sorted_animals)
+    end
+
+    it 'returns sea creatures first' do
+      expect(AnimalSorter.new(animals).to_a.first).to include "marlin"
+    end
+
+    it 'returns land animals second' do
+      expect(AnimalSorter.new(animals).to_a.last).to include "aardvark"
+    end
+  end
+end
