f57387dbea924c9eed97015ca517353887af8758
Katie Hoffman <ktahoffman@gmail.com>
2014-10-07 11:08:21 -0400

    update tests to call on #to_a

--- spec/animal_sorter_spec.rb
+++ spec/animal_sorter_spec.rb
@@ -33,34 +33,34 @@ describe "AnimalSorter" do
     let(:animal_list) { ["marlin", "aardvark", "octopus", "cat", "fish", "elephant"] }
 
     it 'sorts sea and land animals into two arrays' do
-      expect(AnimalSorter.new(animal_list).length).to eq(2)
+      expect(AnimalSorter.new(animal_list).to_a.length).to eq(2)
     end
     
     it 'does not forget about an animal' do
       total_num_of_elements = 0
-      AnimalSorter.new(animal_list).each {|arr| total_num_of_elements += arr.length}
+      AnimalSorter.new(animal_list).to_a.each {|arr| total_num_of_elements += arr.length}
       expect(total_num_of_elements).to eq(6)
     end
 
     it 'returns sea creatures first' do
-      expect(AnimalSorter.new(animal_list)[0]).to include "marlin"
+      expect(AnimalSorter.new(animal_list).to_a[0]).to include "marlin"
     end
 
     it 'returns land animals second' do
-      expect(AnimalSorter.new(animal_list)[1]).to include "aardvark"
+      expect(AnimalSorter.new(animal_list).to_a[1]).to include "aardvark"
     end
 
     it 'includes all sea creatures in the first array' do
-      expect(AnimalSorter.new(animal_list)[0].length).to eq(3)
+      expect(AnimalSorter.new(animal_list).to_a[0].length).to eq(3)
       ["marlin", "octopus", "fish"].each do |sea_creature|
-        expect(AnimalSorter.new(animal_list)[0]).to include(sea_creature)
+        expect(AnimalSorter.new(animal_list).to_a[0]).to include(sea_creature)
       end
     end
 
     it 'includes all land animals in the second array' do
-      expect(AnimalSorter.new(animal_list)[1].length).to eq(3)
+      expect(AnimalSorter.new(animal_list).to_a[1].length).to eq(3)
       ["aardvark", "cat", "elephant"].each do |land_animal|
-        expect(AnimalSorter.new(animal_list)[1]).to include(land_animal)
+        expect(AnimalSorter.new(animal_list).to_a[1]).to include(land_animal)
       end
     end
 
