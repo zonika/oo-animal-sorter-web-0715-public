40042a353d60551e42fae20d6fee3479e6242693
Katie Hoffman <ktahoffman@gmail.com>
2014-10-07 10:55:31 -0400

    flesh out constant tests

--- spec/animal_sorter_spec.rb
+++ spec/animal_sorter_spec.rb
@@ -1,15 +1,29 @@
 describe "AnimalSorter" do
-  it "has a constant array that contains land animals" do
-    expect(AnimalSorter::LAND_ANIMALS.class).to eq(Array)
-    ["aardvark", "cat", "elephant"].each do |land_animal|
-      expect(AnimalSorter::LAND_ANIMALS).to include(land_animal)
+  describe "LAND_ANIMALS" do
+    it "is a constant in the AnimalSorter class" do
+      expect { AnimalSorter::LAND_ANIMALS }.to_not raise_error
+    end
+    it "is an array" do
+      expect(AnimalSorter::LAND_ANIMALS.class).to eq(Array)
+    end
+    it "contains strings of land animals" do
+      ["aardvark", "cat", "elephant"].each do |land_animal|
+        expect(AnimalSorter::LAND_ANIMALS).to include(land_animal)
+      end
     end
   end
 
-  it "has a constant array that contains sea animals" do
-    expect(AnimalSorter::SEA_ANIMALS.class).to eq(Array)
-    ["marlin", "octopus", "fish"].each do |sea_animal|
-      expect(AnimalSorter::SEA_ANIMALS).to include(sea_animal)
+  describe "SEA_ANIMALS" do
+    it "is a constant in the AnimalSorter class" do
+      expect { AnimalSorter::SEA_ANIMALS }.to_not raise_error
+    end
+    it "is an array" do
+      expect(AnimalSorter::SEA_ANIMALS.class).to eq(Array)
+    end
+    it "contains strings of land animals" do
+      ["marlin", "octopus", "fish"].each do |sea_animal|
+        expect(AnimalSorter::SEA_ANIMALS).to include(sea_animal)
+      end
     end
   end
 
