b3eb9f261a14eb1bb9cc26f328e471fa7b1055e2
zonika <zchodosh@conncoll.edu>
2015-08-05 12:59:54 -0400

    done

--- lib/animal_sorter.rb
+++ lib/animal_sorter.rb
@@ -1,9 +1,9 @@
 # Build a class AnimalSorter
 
-# Make sure there are two constants that AnimalSorter knows about, 
+# Make sure there are two constants that AnimalSorter knows about,
 # SEA_ANIMALS and LAND_ANIMALS
 
-# AnimalSorter should be able to accept an array of animals on 
+# AnimalSorter should be able to accept an array of animals on
 # initialization.
 
 # Define a to_a method to account for the species in the test suite.
@@ -12,3 +12,16 @@
 # should include the sea creatures, the second, land animals.
 
 # Read the test suite for an example of a nested array.
+
+class AnimalSorter
+  LAND_ANIMALS=["aardvark","cat","elephant"]  # => ["aardvark", "cat", "elephant"]
+  SEA_ANIMALS=["marlin","octopus","fish"]     # => ["marlin", "octopus", "fish"]
+  def initialize(animals)
+    @amimals=animals
+  end
+  def to_a
+    animals=[SEA_ANIMALS,LAND_ANIMALS]        # => [["marlin", "octopus", "fish"], ["aardvark", "cat", "elephant"]]
+  end
+end
+# animals=AnimalSorter.new                      # => #<AnimalSorter:0x007ff56294c500>
+# animals.to_a                                  # => [["marlin", "octopus", "fish"], ["aardvark", "cat", "elephant"]]
