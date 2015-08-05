6e7e86d005241b0090c48c5feba869825761e8c3
zonika <zchodosh@conncoll.edu>
2015-08-05 13:10:07 -0400

    really done

--- lib/animal_sorter.rb
+++ lib/animal_sorter.rb
@@ -12,16 +12,22 @@
 # should include the sea creatures, the second, land animals.
 
 # Read the test suite for an example of a nested array.
-
 class AnimalSorter
   LAND_ANIMALS=["aardvark","cat","elephant"]  # => ["aardvark", "cat", "elephant"]
   SEA_ANIMALS=["marlin","octopus","fish"]     # => ["marlin", "octopus", "fish"]
-  def initialize(animals)
-    @amimals=animals
+  def initialize(l)
+    @animals=l                                # => ["marlin", "aardvark", "octopus", "cat", "fish", "elephant"]
   end
   def to_a
-    animals=[SEA_ANIMALS,LAND_ANIMALS]        # => [["marlin", "octopus", "fish"], ["aardvark", "cat", "elephant"]]
+    sea=[]                                    # => []
+    land=[]                                   # => []
+    @animals.each do |animal|                 # => ["marlin", "aardvark", "octopus", "cat", "fish", "elephant"]
+      if LAND_ANIMALS.include? animal         # => false, true, false, true, false, true
+        land<<animal                          # => ["aardvark"], ["aardvark", "cat"], ["aardvark", "cat", "elephant"]
+      else
+        sea <<animal                          # => ["marlin"], ["marlin", "octopus"], ["marlin", "octopus", "fish"]
+      end                                     # => ["marlin"], ["aardvark"], ["marlin", "octopus"], ["aardvark", "cat"], ["marlin", "octopus", "fish"], ["aardvark", "cat", "elephant"]
+    end                                       # => ["marlin", "aardvark", "octopus", "cat", "fish", "elephant"]
+    [sea,land]                                # => [["marlin", "octopus", "fish"], ["aardvark", "cat", "elephant"]]
   end
 end
-# animals=AnimalSorter.new                      # => #<AnimalSorter:0x007ff56294c500>
-# animals.to_a                                  # => [["marlin", "octopus", "fish"], ["aardvark", "cat", "elephant"]]
